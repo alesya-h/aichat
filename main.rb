f531f6421bba45a65b77efca2c6da79a4d71e049
Alesya Huzik <public@aguzik.net>
2023-03-27 17:32:35 +1100

    don't use unnecessary fetch

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ require 'colorize'
 require "tty-prompt"
 
 OpenAI.configure do |config|
-    config.access_token = ENV.fetch('OPENAI_API_KEY')
+  config.access_token = ENV['OPENAI_API_KEY']
 end
 
 @client = OpenAI::Client.new
